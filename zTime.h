e390bf983ee9d7121af78b0a19cc3085fbea7f59
leving <thisloving@gmail.com>
2014-07-06 06:41:20 +0800

    time

--- zTime.h
+++ zTime.h
@@ -1,12 +1,12 @@
-#ifndef _zTime_h_
-#define _zTime_h_
+#ifndef _zTimeTool_h_
+#define _zTimeTool_h_
 
 #include <time.h>
 #include <sstream>
 
 #include "zType.h"
 
-namespace zTime
+namespace zTimeTool
 {
 	inline long GetTimeZone()
 	{
@@ -185,7 +185,7 @@ class MinuteCycleEvent : public CycleEvent
 		virtual bool InEventPeriod(const zRTime& current)
 		{
 			struct tm tm;
-			zTime::GetLocalTime(tm, current.sec());	
+			zTimeTool::GetLocalTime(tm, current.sec());	
 
 			int c_sec = tm.tm_sec;
 			int e_begin = second_;
@@ -199,8 +199,8 @@ class MinuteCycleEvent : public CycleEvent
 			struct tm c_tm;
 			struct tm r_tm;
 
-			zTime::GetLocalTime(c_tm, current.sec());
-			zTime::GetLocalTime(r_tm, runtime_);
+			zTimeTool::GetLocalTime(c_tm, current.sec());
+			zTimeTool::GetLocalTime(r_tm, runtime_);
 
 			return (c_tm.tm_year == r_tm.tm_year &&
 					c_tm.tm_yday == r_tm.tm_yday &&
@@ -238,7 +238,7 @@ class HourCycleEvent : public CycleEvent
 		virtual bool InEventPeriod(const zRTime& current)
 		{
 			struct tm tm;
-			zTime::GetLocalTime(tm, current.sec());	
+			zTimeTool::GetLocalTime(tm, current.sec());	
 
 			int c_sec = tm.tm_min * 60 + tm.tm_sec;
 			int e_begin = minute_ * 60 + second_;
@@ -252,8 +252,8 @@ class HourCycleEvent : public CycleEvent
 			struct tm c_tm;
 			struct tm r_tm;
 
-			zTime::GetLocalTime(c_tm, current.sec());
-			zTime::GetLocalTime(r_tm, runtime_);
+			zTimeTool::GetLocalTime(c_tm, current.sec());
+			zTimeTool::GetLocalTime(r_tm, runtime_);
 
 			return (c_tm.tm_year == r_tm.tm_year &&
 					c_tm.tm_yday == r_tm.tm_yday &&
@@ -294,7 +294,7 @@ class DayCycleEvent : public CycleEvent
 		virtual bool InEventPeriod(const zRTime& current)
 		{
 			struct tm tm;
-			zTime::GetLocalTime(tm, current.sec());	
+			zTimeTool::GetLocalTime(tm, current.sec());	
 
 			int c_sec = tm.tm_hour * 3600 + tm.tm_min * 60 + tm.tm_sec;
 			int e_begin = hour_ * 3600 + minute_ * 60 + second_;
@@ -305,7 +305,7 @@ class DayCycleEvent : public CycleEvent
 
 		virtual bool InSameCycle(const zRTime& current)
 		{
-			return zTime::IsSameLocalDay(current.sec(), runtime_);
+			return zTimeTool::IsSameLocalDay(current.sec(), runtime_);
 		}
 
 		virtual void Run(const zRTime& current)
@@ -349,7 +349,7 @@ class WeekCycleEvent : public CycleEvent
 		virtual bool InEventPeriod(const zRTime& current)
 		{
 			struct tm tm;
-			zTime::GetLocalTime(tm, current.sec());	
+			zTimeTool::GetLocalTime(tm, current.sec());	
 
 			int c_sec = tm.tm_hour * 3600 + tm.tm_min * 60 + tm.tm_sec;
 			int e_begin = hour_ * 3600 + minute_ * 60 + second_;
@@ -360,7 +360,7 @@ class WeekCycleEvent : public CycleEvent
 
 		virtual bool InSameCycle(const zRTime& current)
 		{
-			return zTime::IsSameLocalWeek(current.sec(), runtime_);
+			return zTimeTool::IsSameLocalWeek(current.sec(), runtime_);
 		}
 
 		virtual void Run(const zRTime& current)
@@ -402,7 +402,7 @@ class MonthCycleEvent : public CycleEvent
 		virtual bool InEventPeriod(const zRTime& current)
 		{
 			struct tm tm;
-			zTime::GetLocalTime(tm, current.sec());	
+			zTimeTool::GetLocalTime(tm, current.sec());	
 
 			int c_sec = tm.tm_hour * 3600 + tm.tm_min * 60 + tm.tm_sec;
 			int e_begin = hour_ * 3600 + minute_ * 60 + second_;
@@ -413,7 +413,7 @@ class MonthCycleEvent : public CycleEvent
 
 		virtual bool InSameCycle(const zRTime& current)
 		{
-			return zTime::IsSameLocalWeek(current.sec(), runtime_);
+			return zTimeTool::IsSameLocalWeek(current.sec(), runtime_);
 		}
 
 		virtual void Run(const zRTime& current)
@@ -432,5 +432,336 @@ class MonthCycleEvent : public CycleEvent
 		EventFuntor events_;
 };
 
+class zRTime
+{
+	private:
+		unsigned long long _msecs;
 
-#endif  // _zTime_h_
+		inline unsigned long long _now()
+		{
+			unsigned long long retval = 0LL;
+			struct timespec tv;
+			clock_gettime(CLOCK_REALTIME, & tv);
+			retval = tv.tv_sec;
+			retval *= 1000LL;
+			retval += (tv.tv_nsec / 1000000L);
+
+			return retval;
+		}
+
+		inline void NowByDelay(int delay)
+		{
+			_msecs = _now();
+			AddDelay(delay);
+		}
+	
+	public:
+		zRTime(const int delay = 0)
+		{
+			NowByDelay(delay);
+		}
+
+		zRTime(const zRTime& rt)
+		{
+			_msecs = rt._msecs;
+		}
+
+		inline void now()
+		{
+			_msecs = _now();
+		}
+
+		inline unsigned long sec() const
+		{
+			return _msecs / 1000L;
+		}
+
+		inline unsigned long msec() const
+		{
+			return _msecs % 1000L;
+		}
+
+		inline unsigned long long msecs() const
+		{
+			return _msecs;
+		}
+
+		inline void setmsecs(unsigned long long data)
+		{
+			_msecs = data;
+		}
+
+		inline void AddDelay(int delay)
+		{
+			_msecs += delay;
+		}
+
+		inline zRTime& operator= (const zRTime& rt)
+		{
+			_msecs = rt._msecs;
+			return *this;
+		}
+
+		const zRTime& operator+ (const zRTime& rt)
+		{
+			_msecs += rt._msecs();
+			return *this;
+		}
+
+		const zRTime& operator- (const zRTime& rt)
+		{
+			_msecs -= rt._msecs;
+			return *this;
+		}
+
+		bool operator > (const zRTime& rt) const
+		{
+			return _msecs > rt._msecs;
+		}
+
+		bool operator >= (const zRTime& rt) const
+		{
+			return _msecs >= rt._msecs;
+		}
+
+		bool operator < (const zRTime& rt) const
+		{
+			return _msecs < rt._msecs;
+		}
+
+		bool operator <= (const zRTime& rt) const
+		{
+			return _msecs <= rt._msecs;
+		}
+
+		bool operator == (const zRTime& rt) const
+		{
+			return _msecs == rt._msecs;
+		}
+
+		unsigned long long elapse(const zRTime& rt) const
+		{
+			if (rt._msecs > _msecs)
+				return (rt._msecs - _msecs)
+			else
+				return 0LL;
+		}
+
+		static long my_timezone;
+		static const std::string& getLocalTZ();
+		static void save_timezone(std::string& tzstr);
+		static void restore_timezone(const std::string& tzstr);
+		static void GetLocalTime(struct tm& tv1, time_t time_value)
+		{
+			time_value -= my_timezone;
+			gmtime_r(&time_value, &tv1);
+		}
+
+		static WORD GetLocalTime(time_t time_value)
+		{
+			return(time_value - my_timezone) / DAY_SECS;
+		}
+
+		static bool IsSameLocalDay(time_t old_time, time_t new_time)
+		{
+			return (old_time - my_timezone) / DAY_SECS == (new_time - my_timezone) / DAY_SECS;
+		}
+
+		static DWORD GetSecsOfDay(time_t time_value)
+		{
+			return (time_value - my_timezone) % DAY_SECS;
+		}
+
+		static char* asctime(const time_t& t)
+		{
+			struct tm timeinfo;
+			zRTime::GetLocalTime(timeinfo, t);
+
+			return std::asctime(&timeinfo);
+		}
+
+		static char* TimeToStr(const time_t& t)
+		{
+			static char time_str[32];
+			struct tm timeinfo;
+
+			zRTime::GetLocalTime(timeinfo, t);
+
+			bzero(time_str, 32);
+			strftime(time_str, 32, "%Y-%m-%d %H:%M:%S", &timeinfo);
+			return time_str;
+		}
+
+};
+
+class zTime
+{
+	public:
+		zTime()
+		{
+			time(&secs);
+			zRTime::GetLocalTime(tv, secs);
+		}
+
+		zTime(const zTime& ct)
+		{
+			secs = ct.secs;
+			zRTime::GetLocalTime(tv, secs);
+		}
+
+		zRTime(time_t _secs)
+		{
+			secs = _secs;
+			zRTime::GetLocalTime(tv, secs);
+		}
+
+		void now()
+		{
+			time(&secs);
+			zRTime::GetLocalTime(tv, secs);
+		}
+
+		time_t sec() const
+		{
+			return secs;
+		}
+
+		const zTime& operator= (const zTime& rt)
+		{
+			secs = rt.secs;
+			zRTime::GetLocalTime(tv, secs);
+			return *this;
+		}
+
+		const zTime& operator+ (const zTime& rt)
+		{
+			secs += rt.secs;
+			return *this;
+		}
+
+		const zRTime operator- (const zTime& rt)
+		{
+			secs -= rt.secs;
+			return *this;
+		}
+
+		const zRTime operator-= (const time_t s)
+		{
+			secs -= s;
+			return *this;
+		}
+
+		bool operator > (const zTime& rt) const
+		{
+			return secs > rt.secs;
+		}
+
+		bool operator >= (const zTime& rt) const
+		{
+			return secs >= rt.secs;
+		}
+
+		bool operator < (const zTime& rt) const
+		{
+			return secs < rt.secs;
+		}
+
+		bool operator <= (const zTime& rt) const
+		{
+			return secs <= rt.secs;
+		}
+
+		bool operator == (const zTime& rt) const
+		{
+			return secs == rt.secs;
+		}
+
+		time_t elapse(const zTime& rt) const
+		{
+			if (rt.secs > secs)
+				return(rt.secs - secs);
+			else
+				return 0;
+		}
+
+		time_t elapse() const
+		{
+			zTime rt;
+			return (rt.secs - secs)
+		}
+
+		int GetSecs()
+		{
+			return tv.tm_sec;
+		}
+
+		int GetMin()
+		{
+			return tv.tm_min;
+		}
+
+		int GetHour()
+		{
+			return tv.tm_hour;
+		}
+
+		int GetOff()
+		{
+			return tm_gmtoff;
+		}
+
+	private:
+
+		time_t secs;
+
+		struct tm tv;
+};
+
+class Timer
+{
+	public:
+		Timer(const float how_long, const int delay=0) : _long((int)(how_long*1000)), _timer(delay*1000)
+		{}
+
+		void reset(const unsigned long how_long, const zRTime& cur)
+		{
+			_long = how_long;
+			_timer = cur;
+			_timer.AddDelay(_long);
+		}
+
+		void SetTimer(const zRTime& cur)
+		{
+			_timer = cur;
+		}
+
+		Timer(const float how_long, const zRTime& cur) : _long((int)(how_long*1000)), _timer(cur)
+		{
+			_timer.AddDelay(_long);
+		}
+
+		void next(const zRTime& cur)
+		{
+			_timer = cur;
+			_timer.AddDelay(_long);
+		}
+
+		bool operator()(const zRTime& current)
+		{
+			if (_timer <= current)
+			{
+				_timer = current;
+				_timer.AddDelay(_long);
+
+				return true;
+			}
+			
+			return false;
+		}
+	
+	private:
+		int _long;
+		zRTime _timer;
+};
+
+#endif  // _zTimeTool_h_
